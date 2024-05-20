5132ea2778a42ef626de939e02c0f0abc00cd594
brnomosa <omosabria99@gmail.com>
2024-05-20 11:59:58 +0300

    commmplete

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -1,20 +1,30 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
-
-import user from "../data/user";
 import About from "../components/About";
+import logo from "../assets/logo"
 
-test("renders a <p> element with the bio from props", () => {
-  render(<About bio="I made this" links={user.links} />);
-  expect(screen.queryByText("I made this")).toBeInTheDocument();
+test("renders a <aside> element", () => {
+  const { container } = render(<About />);
+  expect(container.querySelector("aside")).toBeInTheDocument();
 });
 
-test("does not render a <p> element if the bio is not included in props", () => {
-  const { container } = render(<About links={user.links} />);
-  expect(container.querySelector("p")).toBeNull();
+test("renders a <img> with the blog logo and alt text of 'blog logo'", () => {
+  render(<About image={logo} />);
+  const img = screen.queryByAltText("blog logo");
+  expect(img).toBeInTheDocument();
+  expect(img.src).toContain(logo);
 });
 
-test("does not render a <p> element if the bio is an empty string", () => {
-  const { container } = render(<About bio="" links={user.links} />);
-  expect(container.querySelector("p")).toBeNull();
+test("uses a default value for the image if no image is passed as a prop", () => {
+  render(<About />);
+  const img = screen.queryByAltText("blog logo");
+  expect(img).toBeInTheDocument();
+  expect(img.src).toContain("https://via.placeholder.com/215");
+});
+
+test("renders a <p> with the about text", () => {
+  render(<About about="About this blog" />);
+  const p = screen.queryByText("About this blog");
+  expect(p).toBeInTheDocument();
+  expect(p.tagName).toBe("P");
 });
